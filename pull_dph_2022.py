a7e0bd155b66c326c0452864bbd606eef2dc39e6
duncanburns2013-dot <duncanburns2013@gmail.com>
2026-02-15 13:00:48 -0500

    DPH FY2022 complete via Socrata API (100002 rows)

---
+++ pull_dph_2022.py
@@ -0,0 +1,20 @@
+﻿import urllib.request, json, csv, time
+
+base = 'https://cthru.data.socrata.com/resource/pegc-naaa.json'
+offset = 0
+all_rows = []
+while True:
+    url = base + '?%24limit=50000&%24offset=' + str(offset) + "&%24where=department_code%3D'DPH'%20AND%20budget_fiscal_year%3D'2022'"
+    data = json.loads(urllib.request.urlopen(url).read())
+    if not data:
+        break
+    all_rows.extend(data)
+    offset += 50000
+    print('Fetched ' + str(len(data)) + ' rows (total: ' + str(len(all_rows)) + ')')
+    time.sleep(1)
+
+with open('data/cthru/dph/dph_fy2022.csv', 'w', newline='', encoding='utf-8') as f:
+    w = csv.DictWriter(f, fieldnames=all_rows[0].keys())
+    w.writeheader()
+    w.writerows(all_rows)
+print('Saved dph_fy2022.csv (' + str(len(all_rows)) + ' rows)')
