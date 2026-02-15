209cd375f3e3642702f8fb15626f86ead478697d
duncanburns2013-dot <duncanburns2013@gmail.com>
2026-02-15 12:42:35 -0500

    DCF vendor payments FY2021-2025

---
+++ peek_dcf.py
@@ -0,0 +1,9 @@
+﻿import csv
+for n in ['10','11','12','13','14']:
+    fname = r'C:\Users\dunca\Downloads\checkbook_data_2026-02-15 (' + n + r').csv'
+    with open(fname, 'r') as f:
+        reader = csv.DictReader(f)
+        row = next(reader)
+        year = row.get('Budget_Fiscal_Year', 'unknown')
+        dept = row.get('Department', 'unknown')
+        print(n + ': FY' + str(year) + ' - ' + dept)
