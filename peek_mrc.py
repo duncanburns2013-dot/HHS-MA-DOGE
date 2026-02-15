f7b37cec327f8f77ff0a1d218f8ec03039c1e1aa
duncanburns2013-dot <duncanburns2013@gmail.com>
2026-02-15 13:11:28 -0500

    MRC vendor payments FY2021-2025

---
+++ peek_mrc.py
@@ -0,0 +1,7 @@
+﻿import csv
+for n in ['21','22','23','24','25']:
+    fname = r'C:\Users\dunca\Downloads\checkbook_data_2026-02-15 (' + n + r').csv'
+    with open(fname, 'r') as f:
+        reader = csv.DictReader(f)
+        row = next(reader)
+        print(n + ': FY' + row.get('Budget_Fiscal_Year','?') + ' - ' + row.get('Department','?'))
